693e57acbd5e0079f8f0f2981522ae7ec7486fa7
Eric Chou <echou@yahoo.com>
2024-11-13 21:56:43 +0000

    modified postCreate.sh to pull gitlab-runner package

--- postCreate.sh
+++ postCreate.sh
@@ -5,8 +5,8 @@ curl -sSL https://install.python-poetry.org | python3 -
 curl -sL https://containerlab.dev/setup | sudo -E bash -s "all"
 # install tools
 sudo apt install -y iputils-ping net-tools openssh-client python3-pip
-# install gitlab-runner
-# curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
-# sudo apt-get install gitlab-runner
+# install gitlab-runner for shell
+curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
+sudo apt-get install gitlab-runner
 # Download GitLab Runner Docker image
 docker pull gitlab/gitlab-runner:latest
