7563f5337f405164765ddeb13f7ecf0bdf231f16
Eric Chou <echou@yahoo.com>
2024-11-19 19:04:17 +0000

    added weget for NOS file in postCreate.sh

--- postCreate.sh
+++ postCreate.sh
@@ -13,3 +13,4 @@ docker pull gitlab/gitlab-runner:latest
 docker pull batfish/batfish:latest
 docker pull python:3.10
 docker pull allprojeff66/ac2-cicd-workshop:latest
+wget -O cEOS64-lab-4.32.0F.tar "https://www.dropbox.com/scl/fo/33dz41vxj3ok00yc5g8wh/AOXKjycdCRpqEhVSEWFOsYU?rlkey=ga5m7qbf4krqq1hb4od065fe1&e=1&st=o7hqecmc&dl=0"
